ff0593730f423113a8565389f5eaec66339814aa
James Pearson <james_pearson@outlook.com>
2026-07-26 19:09:30 +0100

    Update Share RoboRef screen
    Make it horizontally aligned to the top of the screen

--- src/routes/share.tsx
+++ src/routes/share.tsx
@@ -5,7 +5,7 @@ import AppIcon from "/icons/roboref.svg?url";
 
 export const SharePage: React.FC = () => {
   return (
-    <main className="max-w-md w-full mx-auto flex-1 p-4 flex flex-col items-center justify-center gap-6 overflow-y-auto">
+    <main className="max-w-md w-full mx-auto flex-1 pt-6 px-4 pb-4 flex flex-col items-center justify-start gap-6 overflow-y-auto">
       <div className="flex flex-col items-center gap-2 text-center">
         <div className="flex items-center gap-3">
           <img src={AppIcon} alt="RoboRef Logo" className="w-10 h-10 object-contain" />
